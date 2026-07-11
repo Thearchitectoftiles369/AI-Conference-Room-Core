ed428096e5f7ddf6a85d133802f89e6efbb6d96d
Thearchitectoftiles369 <masal230718@gmail.com>
2026-07-11 07:39:21 +0300

    Update conference.py

--- backend/conference.py
+++ backend/conference.py
@@ -1,6 +1,14 @@
-"""
-AI Conference Room Core
-Module: Conference
+from logger import log
 
-Status: Under Development
-"""
+
+class Conference:
+
+    def __init__(self, topic):
+        self.topic = topic
+
+    def start(self):
+        log("Conference started.")
+        print()
+        print(f"Topic: {self.topic}")
+        print("Phase: Understanding the Problem")
+        print()
